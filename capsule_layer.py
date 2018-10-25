0b22c8b46be2cc5886ea4bfff6fc12eff8374c18
Bin Zhou <binzhouchn@gmail.com>
2018-10-25 17:43:31 +0800

    first create on 20181025

--- capsule_layer.py
+++ capsule_layer.py
@@ -9,12 +9,11 @@ import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 USE_CUDA = True
 embedding_dim = 300
-embedding_path = '../save/embedding_matrix.npy' # or False, not use pre-trained-matrix
+embedding_path = '../save/embedding_matrix.npy'  # or False, not use pre-trained-matrix
 use_pretrained_embedding = True
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 gru_len = 128
 Routings = 5
 Num_capsule = 10
@@ -22,103 +21,115 @@ Dim_capsule = 16
 dropout_p = 0.25
 rate_drop_dense = 0.28
 LR = 0.001
-T_epsilon=1e-7
+T_epsilon = 1e-7
 num_classes = 30
 
 
 class Embed_Layer(nn.Module):
-    
     def __init__(self, embedding_matrix=None, vocab_size=None, embedding_dim=300):
         super(Embed_Layer, self).__init__()
-        self.encoder = nn.Embedding(vocab_size+1,embedding_dim)
+        self.encoder = nn.Embedding(vocab_size + 1, embedding_dim)
         if use_pretrained_embedding:
             # self.encoder.weight.data.copy_(t.from_numpy(np.load(embedding_path))) # 方法一，加载np.save的npy文件
-            self.encoder.weight.data.copy_(t.from_numpy(embedding_matrix)) # 方法二
-    def forward(self, x, rate_drop_dense=0.28):
-        return nn.Dropout(p=rate_drop_dense)(self.encoder(x))
+            self.encoder.weight.data.copy_(t.from_numpy(embedding_matrix))  # 方法二
+
+    def forward(self, x, dropout_p=0.25):
+        return nn.Dropout(p=dropout_p)(self.encoder(x))
+
 
 class GRU_Layer(nn.Module):
-    
     def __init__(self):
         super(GRU_Layer, self).__init__()
-        self.gru = nn.GRU(input_size=embedding_dim,
-                         hidden_size=gru_len,
-                         bidirectional=True)     
+        self.gru = nn.GRU(input_size=300,
+                          hidden_size=gru_len,
+                          bidirectional=True)
+
+    # 这步很关键，需要像keras一样用glorot_uniform和orthogonal_uniform初始化参数
+    def init_weights(self):
+        ih = (param.data for name, param in self.named_parameters() if 'weight_ih' in name)
+        hh = (param.data for name, param in self.named_parameters() if 'weight_hh' in name)
+        b = (param.data for name, param in self.named_parameters() if 'bias' in name)
+        for t in ih:
+            nn.init.xavier_uniform_(t)
+        for t in hh:
+            nn.init.orthogonal_(t)
+        for t in b:
+            nn.init.constant_(t, 0)
+
     def forward(self, x):
         return self.gru(x)
 
+
 # core caps_layer with squash func
 class Caps_Layer(nn.Module):
-    def __init__(self, input_dim_capsule=gru_len*2, num_capsule=Num_capsule, dim_capsule=Dim_capsule, \
+    def __init__(self, input_dim_capsule=gru_len * 2, num_capsule=Num_capsule, dim_capsule=Dim_capsule, \
                  routings=Routings, kernel_size=(9, 1), share_weights=True,
-                activation='default',**kwargs):
+                 activation='default', **kwargs):
         super(Caps_Layer, self).__init__(**kwargs)
 
         self.num_capsule = num_capsule
         self.dim_capsule = dim_capsule
         self.routings = routings
-        self.kernel_size = kernel_size # 暂时没用到
+        self.kernel_size = kernel_size  # 暂时没用到
         self.share_weights = share_weights
         if activation == 'default':
             self.activation = self.squash
         else:
             self.activation = nn.ReLU(inplace=True)
-        
+
         if self.share_weights:
             self.W = nn.Parameter(
                 nn.init.xavier_normal_(t.empty(1, input_dim_capsule, self.num_capsule * self.dim_capsule)))
         else:
-            self.W = nn.Parameter(nn.init.xavier_normal_(t.empty(64, input_dim_capsule,self.num_capsule * self.dim_capsule))) #64即batch_size
+            self.W = nn.Parameter(
+                t.randn(BATCH_SIZE, input_dim_capsule, self.num_capsule * self.dim_capsule))  # 64即batch_size
 
     def forward(self, x):
-        
+
         if self.share_weights:
             u_hat_vecs = t.matmul(x, self.W)
         else:
             print('add later')
-            
+
         batch_size = x.size(0)
         input_num_capsule = x.size(1)
         u_hat_vecs = u_hat_vecs.view((batch_size, input_num_capsule,
-                                            self.num_capsule, self.dim_capsule))
-        u_hat_vecs = u_hat_vecs.permute(0, 2, 1, 3) # 转成(batch_size,num_capsule,input_num_capsule,dim_capsule)
-        b = t.zeros_like(u_hat_vecs[:, :, :, 0]) # (batch_size,num_capsule,input_num_capsule)
-        
+                                      self.num_capsule, self.dim_capsule))
+        u_hat_vecs = u_hat_vecs.permute(0, 2, 1, 3)  # 转成(batch_size,num_capsule,input_num_capsule,dim_capsule)
+        b = t.zeros_like(u_hat_vecs[:, :, :, 0])  # (batch_size,num_capsule,input_num_capsule)
+
         for i in range(self.routings):
             b = b.permute(0, 2, 1)
             c = F.softmax(b, dim=2)
             c = c.permute(0, 2, 1)
             b = b.permute(0, 2, 1)
-            outputs = self.activation(t.einsum('bij,bijk->bik', (c, u_hat_vecs))) # batch matrix multiplication
+            outputs = self.activation(t.einsum('bij,bijk->bik', (c, u_hat_vecs)))  # batch matrix multiplication
             # outputs shape (batch_size, num_capsule, dim_capsule)
             if i < self.routings - 1:
-                b = t.einsum('bik,bijk->bij', (outputs, u_hat_vecs)) # batch matrix multiplication
-        return outputs # (batch_size, num_capsule, dim_capsule)
-    
+                b = t.einsum('bik,bijk->bij', (outputs, u_hat_vecs))  # batch matrix multiplication
+        return outputs  # (batch_size, num_capsule, dim_capsule)
+
     # text version of squash, slight different from original one
     def squash(self, x, axis=-1):
-        s_squared_norm  = (x ** 2).sum(axis, keepdim=True)
+        s_squared_norm = (x ** 2).sum(axis, keepdim=True)
         scale = t.sqrt(s_squared_norm + T_epsilon)
         return x / scale
-    
-#     # original one for image decoder
-#     def squash(self, input_tensor):
-#         squared_norm = (input_tensor ** 2).sum(-1, keepdim=True) + T_epsilon
-#         output_tensor = squared_norm *  input_tensor / ((1. + squared_norm) * t.sqrt(squared_norm))
-#         return output_tensor
+
 
 class Dense_Layer(nn.Module):
     def __init__(self):
         super(Dense_Layer, self).__init__()
         self.fc = nn.Sequential(
-            nn.Dropout(p=dropout_p,inplace=True),    
-            nn.Linear(Num_capsule*Dim_capsule, num_classes), # num_capsule*dim_capsule -> num_classes
-            nn.Sigmoid()
+            nn.Dropout(p=dropout_p, inplace=True),
+            nn.Linear(Num_capsule * Dim_capsule, num_classes),  # num_capsule*dim_capsule -> num_classes
+            nn.Softmax(dim=-1)
         )
+
     def forward(self, x):
         batch_size = x.size(0)
         x = x.view(batch_size, -1)
         return self.fc(x)
+
 
 # capsule如果单纯做分类则不需要重构(reconstruction)
 # 如果就用在分类里面，decoder用不到，不需要reconstruction
@@ -128,12 +139,15 @@ class Capsule_Main(nn.Module):
         super(Capsule_Main, self).__init__()
         self.embed_layer = Embed_Layer(embedding_matrix, vocab_size)
         self.gru_layer = GRU_Layer()
+        # 【重要】初始化GRU权重操作，这一步非常关键，acc上升到0.98，如果用默认的uniform初始化则acc一直在0.5左右
+        self.gru_layer.init_weights()
         self.caps_layer = Caps_Layer()
         self.dense_layer = Dense_Layer()
-    
+
     def forward(self, content):
-        embed_output = self.embed_layer(content)
-        gru_output, _ = self.gru_layer(embed_output) # 这个输出是个tuple，一个output(seq_len, batch_size, num_directions * hidden_size)，一个hn
-        caps_output = self.caps_layer(gru_output)
-        output = self.dense_layer(caps_output)
+        content1 = self.embed_layer(content)
+        content2, _ = self.gru_layer(
+            content1)  # 这个输出是个tuple，一个output(seq_len, batch_size, num_directions * hidden_size)，一个hn
+        content3 = self.caps_layer(content2)
+        output = self.dense_layer(content3)
         return output
